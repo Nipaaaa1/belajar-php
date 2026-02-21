7f9f51f59d2894570845a0545d2eba39d87445d9
Nipaaaa1 <frostone224@gmail.com>
2026-02-21 22:58:32 +0000

    learn: arrays

--- index.php
+++ index.php
@@ -1,20 +1,28 @@
 <?php
 
-$number = 504;
-$str = "Hello World";
-$boolean = true;
-$float = 10.5;
+// Arrays
+$array = [1, 2, 3];
 
-var_dump($number);
-var_dump($str);
-var_dump($boolean);
-var_dump($float);
+// Asociative Array
+$aarray = [
+  "name" => "Nipa",
+  "job" => "Programmer"
+];
 
-// String manipulation
-$str1 = " World\n";
-$str2 = "Hello$str1";
-$str3 = "Hello" . $str1;
+var_dump($array);
+var_dump($aarray);
 
-echo $str1;
-echo $str2;
-echo $str3;
+echo $array[0];
+echo $aarray["job"];
+
+// Manipulating array
+
+// Add array
+$array[] = 4;
+
+var_dump($array);
+
+// Delete array item
+unset($array[1]);
+
+var_dump($array);
