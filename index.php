32f9dc48fecbfd2610a1b97c67796de888d712c8
Nipaaaa1 <frostone224@gmail.com>
2026-02-21 22:45:53 +0000

    learn: basic variabels

--- index.php
+++ index.php
@@ -1,2 +1,20 @@
 <?php
-echo "Hello world\n";
+
+$number = 504;
+$str = "Hello World";
+$boolean = true;
+$float = 10.5;
+
+var_dump($number);
+var_dump($str);
+var_dump($boolean);
+var_dump($float);
+
+// String manipulation
+$str1 = " World\n";
+$str2 = "Hello$str1";
+$str3 = "Hello" . $str1;
+
+echo $str1;
+echo $str2;
+echo $str3;
